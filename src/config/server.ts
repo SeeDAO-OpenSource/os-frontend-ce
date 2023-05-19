17185dbf79a34002c654c32fcc97dadbbcc0655d
RueiCiWang <fatfingererr@gmail.com>
2023-05-19 15:03:00 +0800

    feat(user): update new server baseurl

--- src/config/server.ts
+++ src/config/server.ts
@@ -1,15 +1,14 @@
 export default {
-    sendingme: {
-      // baseURL: "http://localhost",
-      // baseURL: "https://seedao-sendingme.azurewebsites.net"
-    },
-    axios: {
-      baseURL: "https://seedaoos.azurewebsites.net",
-      // baseURL: "http://localhost:3000",
-      headers: {
-        "Content-Type": "application/json"
-      }
-    },
-    ledgerDefault: "seedao/prod1"
-  }
-  
+  sendingme: {
+    // baseURL: "http://localhost",
+    // baseURL: "https://seedao-sendingme.azurewebsites.net"
+  },
+  axios: {
+    baseURL: "https://seedaoosv16.azurewebsites.net",
+    // baseURL: "http://localhost:3000",
+    headers: {
+      "Content-Type": "application/json"
+    }
+  },
+  ledgerDefault: "seedao/prod1"
+}
